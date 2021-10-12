6eed645835e3dd3a73055cf028a571d6c1be223e
fancymelonhub <fancycoolemail@gmail.com>
2021-10-12 11:30:17 -0400

    Revert Back To Last DonAlex0 Commit

---
+++ tools/cron/songsCount.php
@@ -0,0 +1,24 @@
+<?
+error_reporting(0);
+chdir(dirname(__FILE__));
+set_time_limit(0);
+$frndlog = "";
+include "../../incl/lib/connection.php";
+$query = $db->prepare("SELECT ID FROM songs");
+$query->execute();
+$result = $query->fetchAll();
+foreach($result as &$songData){
+	$song = $songData["ID"];
+	$query2 = $db->prepare("SELECT count(*) FROM levels WHERE songID = :song");
+	$query2->execute([':song' => $song]);
+	$count = $query2->fetchColumn();
+	$frndlog .= $song . " - " . $count . "\r\n";
+	if($count != 0){
+		echo htmlspecialchars($song,ENT_QUOTES) . " now has $count levels... <br>";
+		$query4 = $db->prepare("UPDATE songs SET levelsCount=:count WHERE ID=:songID");
+		$query4->execute([':count' => $count, ':songID' => $song]);
+	}
+}
+file_put_contents("../logs/snglog.txt",$frndlog);
+echo "<hr>";
+?>
